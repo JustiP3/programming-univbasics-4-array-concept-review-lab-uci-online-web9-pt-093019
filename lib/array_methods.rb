350d39dcee8bfc668a033a8b962b253da227cc20
Justin Phillips <justinphil02@yahoo.com>
2019-10-02 02:22:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,31 +1,27 @@
-
-
-
-
-# def find_element_index(array, value_to_find)
- # index = 0 
-#  while array do
-  #  if value_to_find == array[index]
- #     return index
-#      elsif array[index] == nil 
-#      return nil 
-#    else
-#      index += 1 
- #   end
-#  end
-#end
-
-def find_max_value(array)
-  max = 0 
+ def find_element_index(array, value_to_find)
   index = 0 
-  
-  le = array.length 
-
-  if array[index] > max
-  max = array[index]
+  while array do
+    if value_to_find == array[index]
+     return index
+      elsif array[index] == nil 
+      return nil 
+    else
+      index += 1 
+   end
   end
-
 end
+
+#def find_max_value(array)
+ # max = 0 
+  #index = 0 
+  
+#  le = array.length 
+
+ # if array[index] > max
+  #max = array[index]
+  #end
+
+#end
 
 
 
